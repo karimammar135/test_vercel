ba806d27e6e9b9eeb381a57f154949241339d533
Karim <ka3588828@gmail.com>
2024-01-04 18:23:19 +0200

    add to live postegres database

--- test_vercel/settings.py
+++ test_vercel/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'test_vercel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '4eAfED6gFGAC1Gb-1eDa-cFDAec-*3GG',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '13180',
     }
 }
 
